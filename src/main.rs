6f41aae3696741859ca29d53062bcbe4be774f9f
David Seres <dseres001@gmail.com>
2021-03-21 09:24:12 +0100

    parsers for whitespaces

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,8 @@ fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
 
 #[test]
 fn test_the_letter_a() {
-    assert_eq!( Ok(("bcde",())), the_letter_a("abcde"));    
-    assert_eq!( Err("edcba"), the_letter_a("edcba"));    
+    assert_eq!(Ok(("bcde", ())), the_letter_a("abcde"));
+    assert_eq!(Err("edcba"), the_letter_a("edcba"));
 }
 
 fn match_literal<'a>(expected: &'static str) -> impl Parser<'a, ()> {
@@ -138,26 +138,158 @@ fn test_map() {
     assert_eq!(Ok((", world!", 0xa123)), int_parser.parse("a123, world!"));
 }
 
-fn left<'a, P1, P2, R1, R2>( parser1: P1, parser2: P2) -> impl Parser<'a, R1>
-where   
+fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R1>
+where
     P1: Parser<'a, R1>,
     P2: Parser<'a, R2>,
 {
-    map( pair(parser1, parser2), | (result1, _result2)| result1)
+    map(pair(parser1, parser2), |(result1, _result2)| result1)
 }
 
-
-fn right<'a, P1, P2, R1, R2>( parser1: P1, parser2: P2) -> impl Parser<'a, R2>
-where   
+fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
+where
     P1: Parser<'a, R1>,
     P2: Parser<'a, R2>,
 {
-    map( pair(parser1, parser2), | (_result1, result2)| result2)
+    map(pair(parser1, parser2), |(_result1, result2)| result2)
 }
 
 #[test]
 fn test_left_right() {
-    assert_eq!( Ok(("cd",("ab12".to_string()))), left(identifier, match_literal("!")).parse("ab12!cd"));
-    assert_eq!( Ok(("cd",())), right(identifier, match_literal("!")).parse("ab12!cd"));
+    assert_eq!(
+        Ok(("cd", ("ab12".to_string()))),
+        left(identifier, match_literal("!")).parse("ab12!cd")
+    );
+    assert_eq!(
+        Ok(("cd", ())),
+        right(identifier, match_literal("!")).parse("ab12!cd")
+    );
 }
 
+fn one_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+where
+    P: Parser<'a, A>,
+{
+    move |mut input: &'a str| {
+        let mut result = Vec::<A>::new();
+        match parser.parse(input) {
+            Ok((next_input, next_item)) => {
+                input = next_input;
+                result.push(next_item)
+            }
+            Err(err) => return Err(err),
+        }
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item)
+        }
+        Ok((input, result))
+    }
+    /* It is not working:
+    map(pair( parser, zero_or_more(parser)), |(first, mut others)| {
+        others.insert(0, first);
+        others
+    })
+    */
+}
+
+#[test]
+fn test_one_or_more() {
+    let parser = one_or_more(match_literal("mi"));
+    assert_eq!(Ok(("", vec![(), (), ()])), parser.parse("mimimi"));
+    assert_eq!(Ok(("AAA", vec![(), (), ()])), parser.parse("mimimiAAA"));
+    assert_eq!(Err("nini"), parser.parse("nini"));
+    assert_eq!(Err(""), parser.parse(""));
+}
+
+fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+where
+    P: Parser<'a, A>,
+{
+    move |mut input: &'a str| {
+        let mut result = Vec::<A>::new();
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item)
+        }
+        Ok((input, result))
+    }
+}
+
+#[test]
+fn test_zero_or_more() {
+    let parser = zero_or_more(match_literal("mi"));
+    assert_eq!(Ok(("", vec![(), (), ()])), parser.parse("mimimi"));
+    assert_eq!(Ok(("AAA", vec![(), (), ()])), parser.parse("mimimiAAA"));
+    assert_eq!(Ok(("nini", vec![])), parser.parse("nini"));
+    assert_eq!(Ok(("", vec![])), parser.parse(""));
+}
+
+fn any_char(input: &str) -> ParserResult<char> {
+    match input.chars().next() {
+        Some(next) => Ok((&input[next.len_utf8()..], next)),
+        _ => Err(input),
+    }
+}
+
+#[test]
+fn test_any_char() {
+    assert_eq!(Ok(("rvíztűrő", 'Á')), any_char("Árvíztűrő"));
+}
+
+fn pred<'a, P, A, F>(parser: P, predicate: F) -> impl Parser<'a, A>
+where
+    P: Parser<'a, A>,
+    F: Fn(&A) -> bool,
+{
+    move |input: &'a str| match parser.parse(input) {
+        Ok((next, result)) => {
+            if predicate(&result) {
+                Ok((next, result))
+            } else {
+                Err(input)
+            }
+        }
+        _ => Err(input),
+    }
+}
+
+#[test]
+fn test_pred (){
+    let parser = pred( any_char, |&c| c == 'Á');
+    assert_eq!(Ok(("rvíztűrő", 'Á')), parser.parse("Árvíztűrő"));
+    assert_eq!( Err("árvíztűrő"), parser.parse("árvíztűrő"));
+    assert_eq!( Err(""), parser.parse(""));
+}
+
+fn whitespace_char<'a>() -> impl Parser<'a, char>
+{
+    pred( any_char, |&c| c.is_whitespace())
+}
+
+#[test]
+fn test_whitespace_char() {
+    assert_eq!( Ok(("abc",' ')), whitespace_char().parse(" abc"));
+    assert_eq!( Ok(("abc",'\n')), whitespace_char().parse("\nabc"));
+    assert_eq!( Ok(("abc",'\t')), whitespace_char().parse("\tabc"));
+    assert_eq!( Err("abc"), whitespace_char().parse("abc"));
+}
+
+fn space0<'a>()-> impl Parser<'a, Vec<char> >
+{
+    zero_or_more( whitespace_char())
+}
+
+
+fn space1<'a>()-> impl  Parser<'a, Vec<char> >
+{
+    one_or_more( whitespace_char())
+}
+
+#[test]
+fn test_space() {
+    assert_eq!( Ok(("abc", vec![' ', '\t', ' ', '\n'])), space1().parse(" \t \nabc"));
+    assert_eq!( Ok(("abc", vec![' ', '\t', ' ', '\n'])), space0().parse(" \t \nabc"));
+    assert_eq!( Err("abc"), space1().parse("abc"));
+    assert_eq!( Ok(("abc", vec![])), space0().parse("abc"));
+}
